e049b307ec05800d103693bfdc6be751d1e3cc1f
lishq <2581258925@qq.com>
2019-02-14 14:40:42 +0800

    Site updated: 2019-02-14 14:40:40

--- js/src/crash_cheat.js
+++ js/src/crash_cheat.js
@@ -1,16 +1,1 @@
-var OriginTitle = document.title;
-var titleTime;
-document.addEventListener('visibilitychange', function () {
-    if (document.hidden) {
-        $('[rel="icon"]').attr('href', ".././images/favicon-32x32-next.ico");
-        document.title = '╭(°A°`)╮ 页面崩溃啦 ~';
-        clearTimeout(titleTime);
-    }
-    else {
-        $('[rel="icon"]').attr('href', ".././images/favicon-32x32-next.ico");
-        document.title = '(ฅ>ω<*ฅ) 噫又好了~' + OriginTitle;
-        titleTime = setTimeout(function () {
-            document.title = OriginTitle;
-        }, 2000);
-    }
-});
+var titleTime,OriginTitle=document.title;document.addEventListener("visibilitychange",function(){document.hidden?($('[rel="icon"]').attr("href",".././images/favicon-32x32-next.ico"),document.title="╭(°A°`)╮ 页面崩溃啦 ~",clearTimeout(titleTime)):($('[rel="icon"]').attr("href",".././images/favicon-32x32-next.ico"),document.title="(ฅ>ω<*ฅ) 噫又好了~"+OriginTitle,titleTime=setTimeout(function(){document.title=OriginTitle},2e3))});
